49711c35436d210bce7b71e85e307f941e86cbb7
GhostTal <1760074636@qq.com>
2018-07-15 09:08:39 +0800

    back to WeatherActivity get the select city res
    back to WeatherActivity get the select city res
    --call startActivityForResult

--- app/src/main/java/com/nick/miniweather/SelectCity.java
+++ app/src/main/java/com/nick/miniweather/SelectCity.java
@@ -1,6 +1,7 @@
 package com.nick.miniweather;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -22,6 +23,13 @@ public class SelectCity extends Activity implements View.OnClickListener {
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.title_back:
+                //selectCity的数据是使用Intent返回
+                Intent intent = new Intent();
+                //把返回的数据存入Intent
+                intent.putExtra("cityCode", "101160101");
+                //设置返回数据
+                setResult(RESULT_OK, intent);
+                //关闭Activity
                 finish();
                 break;
             default:
